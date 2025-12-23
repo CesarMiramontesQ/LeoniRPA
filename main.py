e424d76b76b33cad2d5d65e851e07caa776f77fb
Alex Miramontes <alexmiramontes15@gmail.com>
2025-12-23 11:49:54 -0600

    tailwind se agrego

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.gzip import GZipMiddleware
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(GZipMiddleware)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def index(request: Request):
+    return templates.TemplateResponse("base.html", {"request": request})
 
 
 @app.get("/hello/{name}")
